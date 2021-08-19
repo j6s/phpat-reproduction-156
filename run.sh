d5e6cb6e1889ffb9203f00e0b00cc65c527ea46d
Johannes Hertenstein <git@j6s.dev>
2021-08-19 14:38:46 +0200

    Do not use fixed uid in script

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -eo pipefail
 
-docker run --rm -it --user 1000 -v $(pwd):/current -w /current composer install --ignore-platform-reqs
-docker run --rm -it --user 1000 -v $(pwd):/current -w /current php:7.4 ./vendor/bin/phpat
+docker run --rm -it --user $(id -u) -v $(pwd):/current -w /current composer install --ignore-platform-reqs
+docker run --rm -it --user $(id -u) -v $(pwd):/current -w /current php:7.4 ./vendor/bin/phpat
